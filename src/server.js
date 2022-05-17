833305532fa72fb7f16a5cb570f222231eb3a08f
Wille Lee <1992season@gmail.com>
2022-05-17 17:46:19 +0900

    morgan and routers

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 import express from "express";
+import morgan from "morgan";
+/*routers*/
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const handleHome = (req, res, next) => {
-  console.log("home");
-  return res.send("never mind");
-};
-
-app.get("/", handleHome);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+app.use("/", globalRouter);
 
 // the browser never goes anywhere, but receives files that construct pages.
 
