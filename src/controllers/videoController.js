5f49f10eda1eedd42926341dabdf088bd230f56e
Wille Lee <1992season@gmail.com>
2022-05-19 17:58:36 +0900

    conditionals and iterations in pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,14 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Video#1",
+    },
+    {
+      title: "Video#2",
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const editVideo = (req, res) =>
   res.render("edit", { pageTitle: "Edit" });
