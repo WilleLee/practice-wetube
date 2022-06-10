94f381d4145f3dbde132825ab3305c1cf1abb4d5
Wille Lee <1992season@gmail.com>
2022-06-10 20:26:42 +0900

    errored_github

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
+import fetch from "cross-fetch";
 
 export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Create Account" });
@@ -60,6 +61,35 @@ export const postLogin = async (req, res) => {
   req.session.loggedIn = true;
   req.session.user = user;
   return res.redirect("/");
+};
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  const targetUrl = `${baseUrl}?${params}`;
+  return res.redirect(targetUrl);
+};
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const targetUrl = `${baseUrl}?${params}`;
+  const data = await fetch(targetUrl, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const json = await data.json();
+  return res.send(JSON.stringify(json));
 };
 export const editUser = (req, res) => res.send("Edit User");
 export const deleteUser = (req, res) => res.send("Delete User");
--- src/db.js
+++ src/db.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
-console.log(process.env);
-
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
