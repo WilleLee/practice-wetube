ce6bcef2c0f77ae45d995ec57b8820595ad87f65
Wille Lee <1992season@gmail.com>
2022-07-21 18:21:26 +0900

    style ing

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,9 @@ import Video from "../models/Videos";
 import User from "../models/User";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({}).sort({ createdAt: "descending" });
+  const videos = await Video.find({})
+    .sort({ createdAt: "descending" })
+    .populate("owner");
   return res.render("home", { pageTitle: "Home", videos });
 };
 
