fe1f1a0449cf10a527f644831fbd78f10d155d33
Wille Lee <1992season@gmail.com>
2022-07-29 16:48:09 +0900

    time controls of the video player

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,10 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 const volumeRange = document.getElementById("volume");
+const timeline = document.getElementById("timeline");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -43,6 +45,44 @@ const handleVolumeChange = (e) => {
   video.volume = value;
 };
 
+const formatTime = (sec) => {
+  return new Date(sec * 1000).toISOString().substring(11, 19);
+};
+const handleLoadedMetaData = () => {
+  /*
+  const min = Math.floor(Math.floor(video.duration) / 60)
+    .toString()
+    .padStart(2, "0");
+  const sec = (Math.floor(video.duration) % 60).toString().padStart(2, "0");
+  totalTime.innerText = `${min}:${sec}`;
+  */
+  const sec = Math.floor(video.duration);
+  totalTime.innerText = formatTime(sec);
+  timeline.max = sec;
+};
+const handleTimeUpdate = () => {
+  /*
+  const min = Math.floor(Math.floor(video.currentTime) / 60)
+    .toString()
+    .padStart(2, "0");
+  const sec = (Math.floor(video.currentTime) % 60).toString().padStart(2, "0");
+  currentTime.innerText = `${min}:${sec}`;
+  */
+  const sec = Math.floor(video.currentTime);
+  currentTime.innerText = formatTime(sec);
+  timeline.value = sec;
+};
+
+const handleTimelineChange = (e) => {
+  const {
+    target: { value },
+  } = e;
+  video.currentTime = value;
+};
+
 playBtn.addEventListener("click", handlePlayBtn);
 muteBtn.addEventListener("click", handleMuteBtn);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetaData);
+video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimelineChange);
