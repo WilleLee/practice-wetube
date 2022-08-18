2dd0c0c194f4e8d2b601d389d51e35bde9dae546
Wille Lee <1992season@gmail.com>
2022-08-18 18:03:33 +0900

    edit db.js

--- src/db.js
+++ src/db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.DB_URL);
+mongoose.connect(process.env.DB_URL, {
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
